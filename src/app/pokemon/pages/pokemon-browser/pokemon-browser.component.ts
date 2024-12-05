0db99eb9331d8dae7a0346bab9854d3cdc3f9f83
jasonasmk <jasonasmk@gmail.com>
2024-12-06 01:16:22 +0200

    support infinite scroll

--- src/app/pokemon/pages/pokemon-browser/pokemon-browser.component.ts
+++ src/app/pokemon/pages/pokemon-browser/pokemon-browser.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { PokemonViewCard } from '../../components/pokemon-view-card/pokemon-view-card.component';
 import { PokemonService } from '../../services/pokemon.service';
 import { IPokemonServerModel } from '../../models/pokemon.server.model';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { Subject } from 'rxjs';
+import { fromEvent, map, Subject, throttleTime } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 
 @UntilDestroy()
@@ -13,13 +19,42 @@ import { AsyncPipe } from '@angular/common';
   templateUrl: './pokemon-browser.component.html',
   styleUrl: './pokemon-browser.component.scss',
 })
-export class PokemonBrowserPageComponent implements OnInit {
+export class PokemonBrowserPageComponent implements OnInit, AfterViewInit {
   public pokemon$ = new Subject<IPokemonServerModel[]>();
+
+  @ViewChild('list') list!: ElementRef;
+
   constructor(private _pokemonService: PokemonService) {}
 
   public ngOnInit(): void {
     this._pokemonService.pokemons$
       .pipe(untilDestroyed(this))
       .subscribe((newSet) => this.pokemon$.next(newSet));
+  }
+
+  public ngAfterViewInit(): void {
+    this.monitorScrollPosition();
+  }
+
+  private monitorScrollPosition() {
+    fromEvent(this.list.nativeElement, 'scroll')
+      .pipe(
+        throttleTime(500),
+        map(() => this.list.nativeElement),
+        untilDestroyed(this),
+      )
+      .subscribe((container) => {
+        this.getMoreItemsOnScrollEnd(container);
+      });
+  }
+
+  private getMoreItemsOnScrollEnd(container: any) {
+    const threshold = 2000;
+    const position = container.scrollTop + container.clientHeight;
+    const height = container.scrollHeight;
+
+    if (position >= height - threshold) {
+      this._pokemonService.getNextPokemonSet();
+    }
   }
 }
--- src/app/pokemon/services/pokemon.service.ts
+++ src/app/pokemon/services/pokemon.service.ts
@@ -5,6 +5,8 @@ import { LogFactory } from '../../logging/log-factory.service';
 import { Logger } from '../../logging/logger';
 import { IPokemonServerModel } from '../models/pokemon.server.model';
 
+export const POKE_FETCH_SIZE = 20;
+
 @Injectable({ providedIn: 'root' })
 export class PokemonService {
   public get pokemons$() {
@@ -15,6 +17,7 @@ export class PokemonService {
     1,
   );
   private _waitingNextSet = false;
+  private _offset = 0;
   private _logger: Logger;
 
   constructor(
@@ -25,14 +28,14 @@ export class PokemonService {
 
     this._logger.debug('Will fetch and initialize pokemon list');
     this._pokemons$.next([]);
-    this.getNextPokemonSet(0);
+    this.getNextPokemonSet();
   }
 
-  public getNextPokemonSet(offset: number) {
+  public getNextPokemonSet() {
     if (this._waitingNextSet) return;
     this._waitingNextSet = true;
     this._pokemonApiService
-      .getPokemonList(1, offset)
+      .getPokemonList(POKE_FETCH_SIZE, this._offset)
       .pipe(
         tap((ls) => this._logger.debug(`Fetched another ${ls.length} pokemon`)),
         concatMap((pokemonList) => pokemonList),
@@ -45,13 +48,14 @@ export class PokemonService {
           return this.pokemons$.pipe(
             take(1),
             map((oldPokemonList) => oldPokemonList.concat(newPokemonList)),
-          ); // in reality due to network these can come not in order but I consider this out of scope
+          );
         }),
         tap((ls) => this._logger.debug(`Total ${ls.length} pokemon`)),
       )
       .subscribe({
         next: (ls) => {
           this._pokemons$.next(ls);
+          this._offset += POKE_FETCH_SIZE;
           this._waitingNextSet = false;
         },
         error: (err) => {
